6e73a7ee06bac088d622295f47c1963aa4ab8c9b
rajalearn90 <rajaaruna90@gmail.com>
2025-07-18 18:47:45 +0530

    Update kube-prep-steps.ps1

--- kube-prep-steps.ps1
+++ kube-prep-steps.ps1
@@ -1,8 +1,8 @@
 #### Parameters
 
-$keyvaultname = "xxxxx"
-$location = "xxxxx"
-$keyvaultrg = "xxxxxx"
+$keyvaultname = "aviva-akscluster-kv"
+$location = "East US"
+$keyvaultrg = "RG_AvivaProj1"
 
 $sshkeysecret = "xxxxx" # e.g. ekspublickey
 
@@ -11,7 +11,7 @@ $clientidkvsecretname = "xxxxxx" # e.g. spn-id
 $spnclientsecret = "xxxxxx" # generated client secret of the service principle, usually should not store in the repo
 $spnkvsecretname = "xxxxx" # e.g. spn-secret
 $spobjectID = "xxxxx" # service principle's Object ID
-$userobjectid = "xxxxx" # same service principle's Object ID
+$userobjectid = "bc35ed36-da42-4065-9178-6d3206f96c8a" # same service principle's Object ID
 
 
 #### Create Key Vault
@@ -19,8 +19,10 @@ $userobjectid = "xxxxx" # same service principle's Object ID
 New-AzResourceGroup -Name $keyvaultrg -Location $location
 
 New-AzKeyVault -Name $keyvaultname -ResourceGroupName $keyvaultrg -Location $location
+# New-AzKeyVault -Name aviva-akscluster-kv -ResourceGroupName "RG_AvivaProj1" -Location "East US" #vault names are golablly unique
 
 Set-AzKeyVaultAccessPolicy -VaultName $keyvaultname -UserPrincipalName $userobjectid -PermissionsToSecrets get,set,delete,list
+#Set-AzKeyVaultAccessPolicy -VaultName aviva-akscluster-kv -ObjectId "bc35ed36-da42-4065-9178-6d3206f96c8a" -PermissionsToSecrets get,set,delete,list
 
 #### create an ssh key for setting up password-less login between agent nodes.
 
