d1eaf57ad51c1b6c770927c005dc20372ca71852
rajalearn90 <rajaaruna90@gmail.com>
2025-07-21 14:15:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,23 +24,23 @@ data "azurerm_resource_group" "aks_rg" {
 
 resource "azurerm_virtual_network" "aks_vnet" {
   name                = var.aks_vnet_name
-  resource_group_name = azurerm_resource_group.aks_rg.name
-  location            = azurerm_resource_group.aks_rg.location
+  resource_group_name = data.azurerm_resource_group.aks_rg.name
+  location            = data.azurerm_resource_group.aks_rg.location
   address_space       = var.vnetcidr
 } 
 
 resource "azurerm_subnet" "aks_subnet" {
   name                 = "aks_subnet"
-  resource_group_name  = azurerm_resource_group.aks_rg.name
-  virtual_network_name = azurerm_virtual_network.aks_vnet.name
+  resource_group_name  = data.azurerm_resource_group.aks_rg.name
+  virtual_network_name = data.azurerm_virtual_network.aks_vnet.name
   address_prefixes       = var.subnetcidr
 }
 
 
 resource "azurerm_kubernetes_cluster" "aks_cluster" {
   name                = var.cluster_name
-  location            = azurerm_resource_group.aks_rg.location
-  resource_group_name = azurerm_resource_group.aks_rg.name
+  location            = data.azurerm_resource_group.aks_rg.location
+  resource_group_name = data.azurerm_resource_group.aks_rg.name
   dns_prefix          = var.dns_name
 
 
